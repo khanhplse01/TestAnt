cebf0f73e155faa15b6414f7e9cbe233fdedf343
= <=>
2019-12-16 08:27:37 +0700

    add first time

---
+++ src/hellojava/HelloJava.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellojava;
+
+/**
+ *
+ * @author Acer
+ */
+public class HelloJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Xin chào! Welcome to Java");
+        System.out.println("Java is so fun and easy.\nI quit today");
+        // màu cam chuỗi: có gì in đấy
+        // sout + tab : println
+        System.out.println("5+5=" + (5 + 5));
+        // + dùng ghép chuỗi
+        
+    }
+    
+}
